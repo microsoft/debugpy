f1daf0ae5d5dd79969007e9459dfbb516f8f9102
Eric Snow <ericsnowcurrently@gmail.com>
2018-01-22 17:05:32 +0000

    Add a comment to clarify why some tests are Py3-only.

--- tests/debugger_protocol/__init__.py
+++ tests/debugger_protocol/__init__.py
@@ -2,5 +2,8 @@ import sys
 import unittest
 
 
+# The code under the debugger_protocol package isn't used
+# by the debugger (it's used by schema-related tools).  So we don't need
+# to support Python 2.
 if sys.version_info[0] == 2:
     raise unittest.SkipTest('not tested under Python 2')
