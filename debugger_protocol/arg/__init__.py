6c097a4f549a3e078f654c3021b295b3ea1c5895
Eric Snow <ericsnowcurrently@gmail.com>
2018-01-30 00:40:10 +0000

    Add FieldsNamespace.

--- debugger_protocol/arg/__init__.py
+++ debugger_protocol/arg/__init__.py
@@ -1,2 +1,7 @@
 from ._common import NOT_SET, ANY  # noqa
+from ._datatype import FieldsNamespace  # noqa
 from ._decl import Union, Array, Field  # noqa
+from ._errors import (  # noqa
+    ArgumentError,
+    ArgMissingError, IncompleteArgError, ArgTypeMismatchError,
+)
