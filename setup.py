665fec85f58ac6369751ef852297e5bc1b0cba42
Steve Dower <steve.dower@microsoft.com>
2018-02-22 09:09:00 -0800

    Makes extension module optional

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup(name='ptvsd',
         'License :: OSI Approved :: MIT License'],
       packages=['ptvsd'],
       package_data={'ptvsd': list(get_pydevd_package_data())},
-      ext_modules=[Extension('ptvsd.pydevd._pydevd_bundle.pydevd_cython', ['ptvsd/pydevd/_pydevd_bundle/pydevd_cython.c'])],
+      ext_modules=[Extension('ptvsd.pydevd._pydevd_bundle.pydevd_cython',
+                             ['ptvsd/pydevd/_pydevd_bundle/pydevd_cython.c'],
+                             optional=True)],
       )
