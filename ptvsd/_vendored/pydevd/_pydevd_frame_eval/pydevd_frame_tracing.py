b81a0df71d0f08159ff592f52d491d2e811eb053
Fabio Zadrozny <fabiofz@gmail.com>
2018-06-27 18:12:17 -0300

    Add missing 'import os'. (#528)

--- ptvsd/_vendored/pydevd/_pydevd_frame_eval/pydevd_frame_tracing.py
+++ ptvsd/_vendored/pydevd/_pydevd_frame_eval/pydevd_frame_tracing.py
@@ -5,7 +5,7 @@ from _pydev_imps._pydev_saved_modules import threading
 from _pydevd_bundle.pydevd_comm import get_global_debugger, CMD_SET_BREAK, CMD_SET_NEXT_STATEMENT
 from pydevd_file_utils import get_abs_path_real_path_and_base_from_frame, NORM_PATHS_AND_BASE_CONTAINER
 from _pydevd_bundle.pydevd_frame import handle_breakpoint_condition, handle_breakpoint_expression
-
+import os
 
 class DummyTracingHolder:
     dummy_trace_func = None
