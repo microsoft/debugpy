a8b2fee5c8e843ca3cac1a534bbfb0cc632be80f
Karthik Nadig <kanadig@microsoft.com>
2018-07-03 19:21:23 -0700

    Add tests to verify variable sorting (#584)

--- tests/system_tests/test_variables.py
+++ tests/system_tests/test_variables.py
@@ -185,3 +185,115 @@ class VariableLifecycleTests(LifecycleTestsBase):
 
         expected_var_b_eval = {"type": " int", "result": "1"}
         var_b_one_evaluate.resp.body == expected_var_b_eval
+
+    def test_variable_sort_order(self):
+        source = dedent("""
+            b_test = {"spam":"A", "eggs":"B", "abcd":"C"}
+            _b_test = 12
+            __b_test = 13
+            __b_test__ = 14
+
+            a_test = 1
+            _a_test = 2
+            __a_test = 3
+            __a_test__ = 4
+
+            c_test = {1:"one", 2:"two", 10:"ten"}
+            _c_test = 22
+            __c_test = 23
+            __c_test__ = 24
+
+            # <Token>
+            d = 3
+            """)
+        (filename, filepath, env, expected_module, argv,
+         cwd) = self.get_test_info(source)
+
+        bp_line = self.find_line(filepath, 'Token')
+        breakpoints = [{
+            "source": {
+                "path": filepath
+            },
+            "breakpoints": [{
+                "line": bp_line
+            }]
+        }]
+
+        with DebugClient(port=PORT, connecttimeout=CONNECT_TIMEOUT) as editor:
+            adapter, session = editor.host_local_debugger(
+                argv, env=env, cwd=cwd, timeout=CONNECT_TIMEOUT)
+
+            terminated = session.get_awaiter_for_event('terminated')
+            exited = session.get_awaiter_for_event('exited')
+            thread_exit = session.get_awaiter_for_event('thread', lambda msg: msg.body.get("reason", "") == "exited") # noqa
+
+            with session.wait_for_event("stopped") as result:
+                (
+                    _,
+                    _,
+                    _,
+                    _,
+                    _,
+                    _,
+                ) = lifecycle_handshake(
+                    session, "launch", breakpoints=breakpoints)
+            tid = result["msg"].body["threadId"]
+
+            stacktrace = session.send_request("stackTrace", threadId=tid)
+            stacktrace.wait()
+            frame_id = stacktrace.resp.body["stackFrames"][0]["id"]
+            scopes = session.send_request('scopes', frameId=frame_id)
+            scopes.wait()
+            variables_reference = scopes.resp.body["scopes"][0]["variablesReference"] # noqa
+            variables = session.send_request('variables', variablesReference=variables_reference) # noqa
+            variables.wait()
+
+            try:
+                b_dict_var = list(v
+                                  for v in variables.resp.body["variables"]
+                                  if v["name"] == 'b_test')[0]
+                b_dict_var_ref = b_dict_var["variablesReference"]
+                b_dict_var_items = session.send_request('variables', variablesReference=b_dict_var_ref) # noqa
+                b_dict_var_items.wait()
+            except IndexError:
+                b_dict_var_items = None
+
+            try:
+                c_dict_var = list(v
+                                  for v in variables.resp.body["variables"]
+                                  if v["name"] == 'c_test')[0]
+                c_dict_var_ref = c_dict_var["variablesReference"]
+                c_dict_var_items = session.send_request('variables', variablesReference=c_dict_var_ref) # noqa
+                c_dict_var_items.wait()
+            except IndexError:
+                c_dict_var_items = None
+
+            continued = session.get_awaiter_for_event('continued')
+            session.send_request("continue", threadId=tid)
+
+            Awaitable.wait_all(terminated, exited, thread_exit, continued)
+            adapter.wait()
+
+        variables_to_check = list(v["name"]
+                                  for v in variables.resp.body["variables"]
+                                  if v["name"].find('_test') > 0)
+        expected_var_order = ['a_test', 'b_test', 'c_test',
+                              '_a_test', '_b_test', '_c_test',
+                              '__a_test', '__b_test', '__c_test',
+                              '__a_test__', '__b_test__', '__c_test__']
+        self.assertEqual(expected_var_order, variables_to_check)
+
+        # Dict keys are sorted normally, i.e., the '_' rules don't apply
+        # TODO: v["name"][1:5] is needed due to bug #45
+        b_dict_var_keys = list(v["name"][1:5]
+                               for v in b_dict_var_items.resp.body["variables"]
+                               if not v["name"].startswith('__'))  # noqa
+        expected_b_dict_var_keys_order = ['abcd', 'eggs', 'spam']
+        self.assertEqual(b_dict_var_keys, expected_b_dict_var_keys_order)
+
+        # TODO: Numeric dict keys have following issues
+        # bug: #45 and #213
+        # c_dict_var_keys = list(v["name"]
+        #                      for v in c_dict_var_items.resp.body["variables"])  # noqa
+        # expected_c_dict_var_keys_order = ['1', '2', '10', '__len__']
+        # self.assertEqual(c_dict_var_keys, expected_c_dict_var_keys_order)
