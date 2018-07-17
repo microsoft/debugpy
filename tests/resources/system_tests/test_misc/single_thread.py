475a2d1513233b9508706251fb5e5bd7ef7a69eb
Karthik Nadig <kanadig@microsoft.com>
2018-07-16 22:43:07 -0700

    Add thread count tests (#668)
    * Add thread count tests
    
    * Fix Linter
    
    * Adding sleep to allow thread state to be captured accurately

---
+++ tests/resources/system_tests/test_misc/single_thread.py
@@ -0,0 +1,2 @@
+
+print('check here')
--- tests/system_tests/test_misc.py
+++ tests/system_tests/test_misc.py
@@ -1,6 +1,6 @@
 import os
 import os.path
-
+import time
 from tests.helpers.resource import TestResources
 from . import (lifecycle_handshake, LifecycleTestsBase, DebugInfo)
 
@@ -22,3 +22,47 @@ class NoOutputTests(LifecycleTestsBase):
         cwd = os.path.dirname(filename)
         self.run_test_with_no_output(
             DebugInfo(filename=filename, cwd=cwd))
+
+
+class ThreadCountTests(LifecycleTestsBase):
+    def run_test_threads(self, debug_info, bp_filename, bp_line, count):
+        breakpoints = [{
+            'source': {
+                'path': bp_filename
+            },
+            'breakpoints': [{
+                'line': bp_line
+            }]
+        }]
+        with self.start_debugging(debug_info) as dbg:
+            session = dbg.session
+            with session.wait_for_event('stopped') as result:
+                lifecycle_handshake(
+                                    session, debug_info.starttype,
+                                    breakpoints=breakpoints,
+                                    threads=True)
+            # Give extra time for thread state to be captured
+            time.sleep(1)
+            event = result['msg']
+            tid = event.body['threadId']
+            req_threads = session.send_request('threads')
+            req_threads.wait()
+            threads = req_threads.resp.body['threads']
+
+            session.send_request('continue', threadId=tid)
+
+        self.assertEqual(count, len(threads))
+
+    def test_single_thread(self):
+        filename = TEST_FILES.resolve('single_thread.py')
+        cwd = os.path.dirname(filename)
+        self.run_test_threads(
+            DebugInfo(filename=filename, cwd=cwd),
+            bp_filename=filename, bp_line=2, count=1)
+
+    def test_multi_thread(self):
+        filename = TEST_FILES.resolve('three_threads.py')
+        cwd = os.path.dirname(filename)
+        self.run_test_threads(
+            DebugInfo(filename=filename, cwd=cwd),
+            bp_filename=filename, bp_line=22, count=3)
