a8d4299fbfc484ac2ae9731f5775ff99e1e5c4dc
Fabio Zadrozny <fabiofz@gmail.com>
2018-09-18 17:11:07 -0300

    Bump pydevd version to 1.3.3. (#794)

--- ptvsd/_vendored/pydevd/.travis_install_python_deps.sh
+++ ptvsd/_vendored/pydevd/.travis_install_python_deps.sh
@@ -6,10 +6,12 @@ conda install --yes numpy ipython pytest cython psutil
 
 if [ "$PYDEVD_PYTHON_VERSION" = "2.6" ]; then
     conda install --yes pyqt=4
+    pip install pympler==0.5
     # Django 1.7 does not support Python 2.7
 else
     # pytest-xdist not available for python 2.6
     pip install pytest-xdist
+    pip install pympler
 fi
 
 if [ "$PYDEVD_PYTHON_VERSION" = "2.7" ]; then
@@ -23,6 +25,5 @@ if [ "$PYDEVD_PYTHON_VERSION" = "3.5" ]; then
     pip install "django>=1.7,<1.8"
 fi
 
-pip install Pympler
 pip install untangle
 pip install scapy==2.4.0
