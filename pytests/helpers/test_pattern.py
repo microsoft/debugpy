877d83c26a7cab11b518777c8bff83e5908e6fcc
Pavel Minaev <int19h@gmail.com>
2018-10-05 13:37:42 -0700

    Rescue license header from inappropriate refactoring :)

--- pytests/helpers/test_pattern.py
+++ pytests/helpers/test_pattern.py
@@ -1,5 +1,5 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE == the project root
+# Licensed under the MIT License. See LICENSE in the project root
 # for license information.
 
 from __future__ import print_function, with_statement, absolute_import
