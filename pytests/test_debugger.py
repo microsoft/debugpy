bd03d3a523ee3b292e2237536ed4d1988208491d
Karthik Nadig <kanadig@microsoft.com>
2018-11-14 16:40:00 -0800

    Merge into master (#1020)
    * Migrate debugger tests (#968)
    
    * Remove references to old test code (#1019)

--- pytests/test_debugger.py
+++ pytests/test_debugger.py
@@ -1,16 +1,27 @@
 import sys
-import unittest
+import pytest
 
 from ptvsd.debugger import debug, LOCALHOST
 from ptvsd.socket import Address
-from tests.helpers.argshelper import _get_args
 
 
-class DebugTests(unittest.TestCase):
+PROG = 'eggs'
+PORT_ARGS = ['--port', '8888']
+PYDEVD_DEFAULT_ARGS = ['--qt-support=auto']
 
+
+def _get_args(*args, **kwargs):
+    ptvsd_extras = kwargs.get('ptvsd_extras', [])
+    prog = [kwargs.get('prog', PROG)]
+    port = kwargs.get('port', PORT_ARGS)
+    pydevd_args = kwargs.get('pydevd', PYDEVD_DEFAULT_ARGS)
+    return prog + port + ptvsd_extras + pydevd_args + list(args)
+
+
+class TestDebug(object):
+
+    @pytest.fixture()
     def setUp(self):
-        super(DebugTests, self).setUp()
-
         def _make_run(kind):
             def run(addr, name, *args, **kwargs):
                 self._run(kind, addr, name, *args, **kwargs)
@@ -27,7 +38,7 @@ class DebugTests(unittest.TestCase):
         self.args = args
         self.kwargs = kwargs
 
-    def test_module(self):
+    def test_module(self, setUp):
         filename = 'spam'
         _, port = addr = (LOCALHOST, 8888)
         debug_id = 1
@@ -35,13 +46,11 @@ class DebugTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'module',
               _runners=self.runners, _extra=())
 
-        self.assertEqual(self.kind, 'module')
-        self.assertEqual(self.args, (addr, filename))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.kind == 'module'
+        assert self.args == (addr, filename)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_script(self):
+    def test_script(self, setUp):
         filename = 'spam.py'
         _, port = addr = (LOCALHOST, 8888)
         debug_id = 1
@@ -49,13 +58,11 @@ class DebugTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'script',
               _runners=self.runners, _extra=())
 
-        self.assertEqual(self.kind, 'script')
-        self.assertEqual(self.args, (addr, filename))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.kind == 'script'
+        assert self.args == (addr, filename)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_code(self):
+    def test_code(self, setUp):
         filename = "print('spam')"
         _, port = addr = (LOCALHOST, 8888)
         debug_id = 1
@@ -63,13 +70,11 @@ class DebugTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'code',
               _runners=self.runners, _extra=())
 
-        self.assertEqual(self.kind, 'code')
-        self.assertEqual(self.args, (addr, filename))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.kind == 'code'
+        assert self.args == (addr, filename)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_unsupported(self):
+    def test_unsupported(self, setUp):
         filename = 'spam'
         _, port = addr = (LOCALHOST, 8888)
         debug_id = 1
@@ -77,13 +82,11 @@ class DebugTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, '???',
               _runners=self.runners, _extra=())
 
-        self.assertIs(self.kind, None)
-        self.assertEqual(self.args, (addr, filename))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.kind is None
+        assert self.args == (addr, filename)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_extra_sys_argv(self):
+    def test_extra_sys_argv(self, setUp):
         filename = 'spam.py'
         _, port = addr = (LOCALHOST, 8888)
         debug_id = 1
@@ -92,31 +95,27 @@ class DebugTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'script',
               _runners=self.runners, _extra=extra)
 
-        self.assertEqual(self.args, (addr, filename, '--eggs', 'abc'))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.args == (addr, filename, '--eggs', 'abc')
+        assert self.kwargs == {'singlesession': True}
 
 
-class IntegrationTests(unittest.TestCase):
+class TestIntegration(object):
 
+    @pytest.fixture(scope='function')
     def setUp(self):
-        super(IntegrationTests, self).setUp()
         self.argv = None
         self.addr = None
         self.kwargs = None
         self._sys_argv = list(sys.argv)
-
-    def tearDown(self):
+        yield
         sys.argv[:] = self._sys_argv
-        super(IntegrationTests, self).tearDown()
 
     def _run(self, argv, addr, **kwargs):
         self.argv = argv
         self.addr = addr
         self.kwargs = kwargs
 
-    def test_module(self):
+    def test_module(self, setUp):
         filename = 'spam'
         port = 8888
         debug_id = 1
@@ -125,16 +124,11 @@ class IntegrationTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'module',
               _run=self._run, _prog='eggs')
 
-        self.assertEqual(self.argv,
-                         _get_args('--module', '--file', 'spam:',
-                                   ptvsd_extras=['--client', LOCALHOST]))
+        assert self.argv == _get_args('--module', '--file', 'spam:', ptvsd_extras=['--client', LOCALHOST])
+        assert self.addr == Address.as_client(None, port)
+        assert self.kwargs == {'singlesession': True}
 
-        self.assertEqual(self.addr, Address.as_client(None, port))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
-
-    def test_script(self):
+    def test_script(self, setUp):
         filename = 'spam.py'
         port = 8888
         debug_id = 1
@@ -143,15 +137,11 @@ class IntegrationTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'script',
               _run=self._run, _prog='eggs')
 
-        self.assertEqual(self.argv,
-                         _get_args('--file', 'spam.py',
-                                   ptvsd_extras=['--client', LOCALHOST]))
-        self.assertEqual(self.addr, Address.as_client(None, port))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.argv == _get_args('--file', 'spam.py', ptvsd_extras=['--client', LOCALHOST])
+        assert self.addr == Address.as_client(None, port)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_code(self):
+    def test_code(self, setUp):
         filename = "print('spam')"
         port = 8888
         debug_id = 1
@@ -160,15 +150,11 @@ class IntegrationTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'code',
               _run=self._run, _prog='eggs')
 
-        self.assertEqual(self.argv,
-                         _get_args('--file', filename,
-                                   ptvsd_extras=['--client', LOCALHOST]))
-        self.assertEqual(self.addr, Address.as_client(None, port))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.argv == _get_args('--file', filename, ptvsd_extras=['--client', LOCALHOST])
+        assert self.addr == Address.as_client(None, port)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_unsupported(self):
+    def test_unsupported(self, setUp):
         filename = 'spam'
         port = 8888
         debug_id = 1
@@ -177,15 +163,11 @@ class IntegrationTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, '???',
               _run=self._run, _prog='eggs')
 
-        self.assertEqual(self.argv,
-                         _get_args('--file', 'spam',
-                                   ptvsd_extras=['--client', LOCALHOST]))
-        self.assertEqual(self.addr, Address.as_client(None, port))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.argv == _get_args('--file', 'spam', ptvsd_extras=['--client', LOCALHOST])
+        assert self.addr == Address.as_client(None, port)
+        assert self.kwargs == {'singlesession': True}
 
-    def test_extra_sys_argv(self):
+    def test_extra_sys_argv(self, setUp):
         filename = 'spam.py'
         port = 8888
         debug_id = 1
@@ -194,10 +176,6 @@ class IntegrationTests(unittest.TestCase):
         debug(filename, port, debug_id, debug_options, 'script',
               _run=self._run, _prog='eggs')
 
-        self.assertEqual(self.argv,
-                         _get_args('--file', 'spam.py', '--abc', 'xyz', '42',
-                                   ptvsd_extras=['--client', LOCALHOST]))
-        self.assertEqual(self.addr, Address.as_client(None, port))
-        self.assertEqual(self.kwargs, {
-            'singlesession': True,
-        })
+        assert self.argv == _get_args('--file', 'spam.py', '--abc', 'xyz', '42', ptvsd_extras=['--client', LOCALHOST])
+        assert self.addr == Address.as_client(None, port)
+        assert self.kwargs == {'singlesession': True}
