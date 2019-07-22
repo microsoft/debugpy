7973e5beacb5cbad2b972262f5b6bf4c81b75706
Pavel Minaev <int19h@gmail.com>
2019-07-22 12:21:31 -0700

    Fix sys.path patching when running ptvsd without -m.

--- src/ptvsd/__main__.py
+++ src/ptvsd/__main__.py
@@ -4,8 +4,50 @@
 
 from __future__ import absolute_import, print_function, unicode_literals
 
-from ptvsd.server.__main__ import main
+import os.path
+import sys
+
+__all__ = []
+
+# Force absolute path on Python 2.
+__file__ = os.path.abspath(__file__)
 
 
 if __name__ == "__main__":
+    # ptvsd can also be invoked directly rather than via -m. In this case, the first
+    # entry on sys.path is the one added automatically by Python for the directory
+    # containing this file. This means that import ptvsd will not work, since we need
+    # the parent directory of ptvsd/ to be in sys.path, rather than ptvsd/ itself.
+    #
+    # The other issue is that many other absolute imports will break, because they
+    # will be resolved relative to ptvsd/ - e.g. `import debugger` will then try
+    # to import ptvsd/debugger.py.
+    #
+    # To fix both, we need to replace the automatically added entry such that it points
+    # at parent directory of ptvsd/ instead of ptvsd/ itself, import ptvsd with that
+    # in sys.path, and then remove the first entry entry altogether, so that it doesn't
+    # affect any further imports we might do. For example, suppose the user did:
+    #
+    #   python /foo/bar/ptvsd ...
+    #
+    # At the beginning of this script, sys.path will contain "/foo/bar/ptvsd" as the
+    # first entry. What we want is to replace it with "/foo/bar', then import ptvsd
+    # with that in effect, and then remove the replaced entry before any more
+    # code runs. The imported ptvsd module will remain in sys.modules, and thus all
+    # future imports of it or its submodules will resolve accordingly.
+    if "ptvsd" not in sys.modules:
+        # Do not use dirname() to walk up - this can be a relative path, e.g. ".".
+        sys.path[0] = sys.path[0] + "/../"
+        __import__("ptvsd")
+        del sys.path[0]
+
+    if "pydevd" not in sys.modules:
+        assert "threading" not in sys.modules
+
+    # Ensure that both packages are loaded before ptvsd.server.__main__ is imported -
+    # this simplifies imports in the latter.
+    import ptvsd  # noqa
+    import pydevd  # noqa
+
+    from ptvsd.server.__main__ import main
     main()
--- src/ptvsd/server/__main__.py
+++ src/ptvsd/server/__main__.py
@@ -7,51 +7,21 @@ from __future__ import absolute_import, print_function, with_statement
 import numbers
 import os.path
 import runpy
-import sys
 import site
+import sys
 import sysconfig
+import threading
 
-# ptvsd can also be invoked directly rather than via -m. In this case, the
-# first entry on sys.path is the one added automatically by Python for the
-# directory containing this file. This means that 1) import ptvsd will not
-# work, since we need the parent directory of ptvsd/ to be on path, rather
-# than ptvsd/ itself, and 2) many other absolute imports will break, because
-# they will be resolved relative to ptvsd/ - e.g. import socket will then
-# try to import ptvsd/socket.py!
-#
-# To fix this, we need to replace the automatically added entry such that it
-# points at the parent directory instead, import ptvsd from that directory,
-# and then remove than entry altogether so that it doesn't affect any further
-# imports. For example, suppose the user did:
-#
-#   python /foo/bar/ptvsd ...
-#
-# At the beginning of this script, sys.path will contain '/foo/bar/ptvsd' as
-# the first entry. What we want is to replace it with '/foo/bar', then import
-# ptvsd with that in effect, and then remove it before continuing execution.
-if __name__ == '__main__' and 'ptvsd' not in sys.modules:
-    sys.path[0] = os.path.dirname(sys.path[0])
-    import ptvsd  # noqa
-    del sys.path[0]
+# If this wasn't already loaded, some stdlib modules above might have had loaded
+# threading before pydevd could do the necessary detours.
+assert "pydevd" in sys.modules
 
 import pydevd
-
-import threading  # Import after pydevd.
-
 import ptvsd.server._remote
 import ptvsd.server.options
 import ptvsd.server.runner
 from ptvsd.server.multiproc import listen_for_subprocesses
 
-# When forming the command line involving __main__.py, it might be tempting to
-# import it as a module, and then use its __file__. However, that does not work
-# reliably, because __file__ can be a relative path - and when it is relative,
-# that's relative to the current directory at the time import was done, which
-# may be different from the current directory at the time the path is used.
-#
-# So, to be able to correctly locate the script at any point, we compute the
-# absolute path at import time.
-__file__ = os.path.abspath(__file__)
 
 TARGET = '<filename> | -m <module> | -c <code> | --pid <pid>'
 
