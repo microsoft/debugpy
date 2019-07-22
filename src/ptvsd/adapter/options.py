ffbbf64832d0169c6a18265cb38a5eeb6ee1a956
Pavel Minaev <int19h@gmail.com>
2019-07-22 12:25:03 -0700

    Add docstring to ptvsd.adapter.options

--- src/ptvsd/adapter/options.py
+++ src/ptvsd/adapter/options.py
@@ -2,6 +2,12 @@
 # Licensed under the MIT License. See LICENSE in the project root
 # for license information.
 
+from __future__ import absolute_import, print_function, unicode_literals
+
+"""Global adapter options that are set via command line, environment variables,
+or configuartion files.
+"""
+
 
 host = "127.0.0.1"
 """Default host name for the debug server"""
