a6983409856dbb209410e8956319361628bb5a9b
Pavel Minaev <int19h@gmail.com>
2019-08-12 14:14:39 -0700

    Add .cmd script to clean various Python-related build and run artifacts in the repo - .pyc, .pyo, .pyd, __pycache__.

---
+++ clean.cmd
@@ -0,0 +1,15 @@
+@echo off
+
+pushd %~dp0
+del /s /q *.pyc
+del /s /q *.pyo
+for /d /r %%i in (__pycache__.*) do rd %%i
+popd
+
+pushd %~dp0\src
+del /s /q *.pyd
+popd
+
+pushd %~dp0\tests
+del /s /q *.pyd
+popd
