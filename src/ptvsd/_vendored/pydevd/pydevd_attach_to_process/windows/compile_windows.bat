9c40387d036f5a5276f2106037c6e0f8b4abbaae
Fabio Zadrozny <fabiofz@gmail.com>
2020-01-09 17:06:19 -0300

    Add symbols for the shipped binaries. Fixes #2032 (#2033)

--- src/ptvsd/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
+++ src/ptvsd/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
@@ -8,8 +8,9 @@ cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /LD /MD /D BITS_32 run_code_on_dllmain.cpp
 copy run_code_on_dllmain_x86.dll ..\run_code_on_dllmain_x86.dll /Y
 copy run_code_on_dllmain_x86.pdb ..\run_code_on_dllmain_x86.pdb /Y
 
-cl /EHsc inject_dll.cpp /link /out:inject_dll_x86.exe
+cl /EHsc /Zi /O1 inject_dll.cpp /link /DEBUG  /OPT:REF /OPT:ICF /out:inject_dll_x86.exe
 copy inject_dll_x86.exe ..\inject_dll_x86.exe /Y
+copy inject_dll_x86.pdb ..\inject_dll_x86.pdb /Y
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /W3 /LD /MD attach.cpp /link /DEBUG /OPT:REF /OPT:ICF /out:attach_amd64.dll
@@ -20,8 +21,9 @@ cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /LD /MD /D BITS_64 run_code_on_dllmain.cpp
 copy run_code_on_dllmain_amd64.dll ..\run_code_on_dllmain_amd64.dll /Y
 copy run_code_on_dllmain_amd64.pdb ..\run_code_on_dllmain_amd64.pdb /Y
 
-cl /EHsc inject_dll.cpp /link /out:inject_dll_amd64.exe
+cl /EHsc /Zi /O1 inject_dll.cpp /link /DEBUG  /OPT:REF /OPT:ICF /out:inject_dll_amd64.exe
 copy inject_dll_amd64.exe ..\inject_dll_amd64.exe /Y
+copy inject_dll_amd64.pdb ..\inject_dll_amd64.pdb /Y
 
 del *.exe
 del *.lib
