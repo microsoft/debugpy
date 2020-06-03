2f62f796528a650c4ec82090f72eea584d9f7790
Fabio Zadrozny <fabiofz@gmail.com>
2020-06-03 12:55:42 -0300

    If no patching is required for subprocess, return original arguments. Fixes #263

--- src/debugpy/_vendored/pydevd/pydevd.py
+++ src/debugpy/_vendored/pydevd/pydevd.py
@@ -2757,6 +2757,7 @@ class DispatchReader(ReaderThread):
 
         ReaderThread.__init__(
             self,
+            get_global_debugger(),
             self.dispatcher.client,
             PyDevJsonCommandProcessor=PyDevJsonCommandProcessor,
             process_net_command=process_net_command,
