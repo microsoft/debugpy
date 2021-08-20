b10d93dbbdd41278172c8161b498764839655ce1
Pavel Minaev <me@int19h.org>
2021-08-19 18:01:51 -0700

    Build attach_linux_*.so for manylinux1 using the official containers from PyPA.

--- clean.cmd
+++ clean.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 pushd %~dp0
+rd /s /q build dist
 del /s /q *.pyc
 del /s /q *.pyo
 for /d /r %%i in (__pycache__.*) do rd "%%i"
@@ -8,8 +9,10 @@ popd
 
 pushd %~dp0\src
 del /s /q *.pyd
+del /s /q *-linux-gnu.so
 popd
 
 pushd %~dp0\tests
 del /s /q *.pyd
+del /s /q *-linux-gnu.so
 popd
