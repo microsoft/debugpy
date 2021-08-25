68d82b02b219c9623e383b2db6d504ad7ae6cc38
Pavel Minaev <me@int19h.org>
2021-08-25 08:05:00 -0700

    Fix #155: manylinux wheels don't pass auditwheel checks
    For platform-specific wheels, do not include attach-to-PID binaries for other platforms.

--- setup.py
+++ setup.py
@@ -9,35 +9,10 @@ import os.path
 import subprocess
 import sys
 
-
-pure = None
-if "--pure" in sys.argv:
-    pure = True
-    sys.argv.remove("--pure")
-elif "--universal" in sys.argv:
-    pure = True
-elif "--abi" in sys.argv:
-    pure = False
-    sys.argv.remove("--abi")
-
-
-# bdist_wheel determines whether the package is pure or not based on ext_modules.
-# However, all pydevd native modules are prebuilt and packaged as data, so they
-# should not be in the list.
-#
-# The proper way to handle this is by overriding has_ext_modules. However, due to
-# https://bugs.python.org/issue32957, in setuptools 57.0.0 and below, it is not
-# always called when it should be, with ext_modules tested directly instead.
-#
-# So, for non-pure builds, we provide a customized empty list for ext_modules that
-# tests as truthful - this causes the package to be treated as non-pure on all
-# relevant setuptools versions.
-class ExtModules(list):
-    def __bool__(self):
-        return not pure
-
-
 from setuptools import setup  # noqa
+from distutils.command.build import build as _build
+from distutils.command.build_py import build_py as _build_py
+
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import versioneer  # noqa
@@ -82,6 +57,80 @@ def iter_vendored_files():
             yield filename
 
 
+# bdist_wheel determines whether the package is pure or not based on ext_modules.
+# However, all pydevd native modules are prebuilt and packaged as data, so they
+# should not be in the list.
+#
+# The proper way to handle this is by overriding has_ext_modules. However, due to
+# https://bugs.python.org/issue32957, in setuptools 57.0.0 and below, it is not
+# always called when it should be, with ext_modules tested directly instead.
+#
+# So, for non-pure builds, we provide a customized empty list for ext_modules that
+# tests as truthful - this causes the package to be treated as non-pure on all
+# relevant setuptools versions.
+class ExtModules(list):
+    def __bool__(self):
+        return True
+
+
+# Filter platform-specific binaries in data files for binary builds.
+# If a data file is not in this list, it is assumed to be applicable to all platforms.
+
+win_all = {"win32", "win-amd64"}
+vendored_binaries = dict(
+    (os.path.normpath(k), v)
+    for k, v in {
+        "pydevd/pydevd_attach_to_process/attach_x86.dll": win_all,
+        "pydevd/pydevd_attach_to_process/attach_x86.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/attach_amd64.dll": win_all,
+        "pydevd/pydevd_attach_to_process/attach_amd64.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/inject_dll_x86.exe": win_all,
+        "pydevd/pydevd_attach_to_process/inject_dll_x86.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/inject_dll_amd64.exe": win_all,
+        "pydevd/pydevd_attach_to_process/inject_dll_amd64.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/run_code_on_dllmain_x86.dll": win_all,
+        "pydevd/pydevd_attach_to_process/run_code_on_dllmain_x86.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/run_code_on_dllmain_amd64.dll": win_all,
+        "pydevd/pydevd_attach_to_process/run_code_on_dllmain_amd64.pdb": win_all,
+        "pydevd/pydevd_attach_to_process/attach_linux_x86.so": {"linux-i686"},
+        "pydevd/pydevd_attach_to_process/attach_linux_amd64.so": {"linux-x86_64"},
+        "pydevd/pydevd_attach_to_process/attach_x86_64.dylib": {"macosx_10_14_x86_64"},
+    }.items()
+)
+
+
+class build(_build):
+    def finalize_options(self):
+        # Mark all packages as pure if requested to build a universal wheel.
+        # Otherwise, bdist_wheel will silently ignore the request.
+        bdist_wheel = self.distribution.get_command_obj("bdist_wheel", 0)
+        if bdist_wheel is not None and bdist_wheel.universal:
+            self.distribution.ext_modules = []
+            self.distribution.has_ext_modules = lambda: False
+        _build.finalize_options(self)
+
+
+class build_py(_build_py):
+    def finalize_options(self):
+        _build_py.finalize_options(self)
+
+        # Don't filter anything for universal wheels.
+        if not self.distribution.ext_modules:
+            return
+
+        plat = self.get_finalized_command("build").plat_name
+        data_files = self.data_files
+
+        def is_applicable(filename):
+            filename = os.path.normpath(filename)
+            return plat in vendored_binaries.get(filename, {plat})
+
+        self.data_files = [
+            (package, src_dir, build_dir, list(filter(is_applicable, filenames)))
+            for package, src_dir, build_dir, filenames in data_files
+        ]
+
+
 with open("DESCRIPTION.md", "r") as fh:
     long_description = fh.read()
 
@@ -94,6 +143,9 @@ if __name__ == "__main__":
     platforms = get_buildplatform()
     if platforms is not None:
         extras["platforms"] = platforms
+
+    cmds = versioneer.get_cmdclass()
+    cmds.update(build=build, build_py=build_py)
 
     setup(
         name="debugpy",
@@ -135,7 +187,7 @@ if __name__ == "__main__":
             "debugpy._vendored": list(iter_vendored_files()),
         },
         ext_modules=ExtModules(),
-        has_ext_modules=lambda: not pure,
-        cmdclass=versioneer.get_cmdclass(),
+        has_ext_modules=lambda: True,
+        cmdclass=cmds,
         **extras
     )
