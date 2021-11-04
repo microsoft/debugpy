a5e05bdce856fdf65c4614e0a67790baa728533c
Fabio Zadrozny <fabiofz@gmail.com>
2021-11-04 09:42:05 -0300

    Sync pydevd to 2.7.0.

--- src/debugpy/_vendored/pydevd/_pydev_bundle/pydev_log.py
+++ src/debugpy/_vendored/pydevd/_pydev_bundle/pydev_log.py
@@ -1,4 +1,4 @@
-from _pydevd_bundle.pydevd_constants import DebugInfoHolder, SHOW_COMPILE_CYTHON_COMMAND_LINE, NULL
+from _pydevd_bundle.pydevd_constants import DebugInfoHolder, SHOW_COMPILE_CYTHON_COMMAND_LINE, NULL, LOG_TIME
 from contextlib import contextmanager
 import traceback
 import os
@@ -90,6 +90,10 @@ def log_context(trace_level, stream):
         _LoggingGlobals._debug_stream_initialized = original_initialized
 
 
+import time
+_last_log_time = time.time()
+
+
 def _pydevd_log(level, msg, *args):
     '''
     Levels are:
@@ -107,7 +111,15 @@ def _pydevd_log(level, msg, *args):
                     msg = msg % args
             except:
                 msg = '%s - %s' % (msg, args)
-            msg = '%s\n' % (msg,)
+
+            if LOG_TIME:
+                global _last_log_time
+                new_log_time = time.time()
+                time_diff = new_log_time - _last_log_time
+                _last_log_time = new_log_time
+                msg = '%.2fs - %s\n' % (time_diff, msg,)
+            else:
+                msg = '%s\n' % (msg,)
             try:
                 try:
                     initialize_debug_stream()  # Do it as late as possible
--- src/debugpy/_vendored/pydevd/pydevd.py
+++ src/debugpy/_vendored/pydevd/pydevd.py
@@ -98,7 +98,7 @@ from _pydevd_bundle.pydevd_thread_lifecycle import suspend_all_threads, mark_thr
 if USE_CUSTOM_SYS_CURRENT_FRAMES_MAP:
     from _pydevd_bundle.pydevd_constants import constructed_tid_to_last_frame
 
-__version_info__ = (2, 6, 0)
+__version_info__ = (2, 7, 0)
 __version_info_str__ = []
 for v in __version_info__:
     __version_info_str__.append(str(v))
