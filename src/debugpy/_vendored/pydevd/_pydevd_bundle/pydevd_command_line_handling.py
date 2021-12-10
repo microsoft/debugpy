120476ec25ae41a749db2b8859f55d07a912af2b
Fabio Zadrozny <fabiofz@gmail.com>
2021-12-10 18:28:05 -0300

    Don't use --module from the setup during monkey-patching subprocess args. Fixes #154

--- src/debugpy/_vendored/pydevd/_pydevd_bundle/pydevd_command_line_handling.py
+++ src/debugpy/_vendored/pydevd/_pydevd_bundle/pydevd_command_line_handling.py
@@ -103,17 +103,22 @@ def get_pydevd_file():
     return f
 
 
-def setup_to_argv(setup):
+def setup_to_argv(setup, skip_names=None):
     '''
     :param dict setup:
         A dict previously gotten from process_command_line.
 
+    :param set skip_names:
+        The names in the setup which shouldn't be converted to argv.
+
     :note: does not handle --file nor --DEBUG.
     '''
+    if skip_names is None:
+        skip_names = set()
     ret = [get_pydevd_file()]
 
     for handler in ACCEPTED_ARG_HANDLERS:
-        if handler.arg_name in setup:
+        if handler.arg_name in setup and handler.arg_name not in skip_names:
             handler.to_argv(ret, setup)
     return ret
 
