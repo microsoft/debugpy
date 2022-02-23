52b052a38b2b72221144341ae7def91b80f34e57
Pavel Minaev <pminaev@microsoft.com>
2022-02-23 15:40:31 -0800

    Remove Python 2.7 from supported implementations and update versions of test packages.

--- setup.py
+++ setup.py
@@ -155,10 +155,9 @@ if __name__ == "__main__":
         author="Microsoft Corporation",
         author_email="ptvshelp@microsoft.com",
         url="https://aka.ms/debugpy",
-        python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*",
+        python_requires=">=3.5",
         classifiers=[
             "Development Status :: 5 - Production/Stable",
-            "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3.5",
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
