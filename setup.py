42853a99c4d3d4d27e4960c8894d780d4f091d68
Pavel Minaev <pminaev@microsoft.com>
2024-01-25 13:20:09 -0800

    Add Python 3.12 as officially supported.

--- setup.py
+++ setup.py
@@ -169,6 +169,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.9",
             "Programming Language :: Python :: 3.10",
             "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
             "Topic :: Software Development :: Debuggers",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: MacOS",
