e5988e47b4c7a4fc22f8b80779903f74e15f7a0d
Adam Yoblick <Adam.Yoblick@microsoft.com>
2024-04-16 18:23:18 -0500

    add profile flag when linking to fix apiscan errors

--- src/debugpy/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
+++ src/debugpy/_vendored/pydevd/pydevd_attach_to_process/windows/compile_windows.bat
@@ -16,7 +16,7 @@ cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /W3 /LD /MD /D BITS_32 /Qspectre run_code_
 copy run_code_on_dllmain_x86.dll ..\run_code_on_dllmain_x86.dll /Y
 copy run_code_on_dllmain_x86.pdb ..\run_code_on_dllmain_x86.pdb /Y
 
-cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /DEBUG /OPT:REF /OPT:ICF /GUARD:CF /out:inject_dll_x86.exe
+cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /PROFILE /GUARD:CF /out:inject_dll_x86.exe
 copy inject_dll_x86.exe ..\inject_dll_x86.exe /Y
 copy inject_dll_x86.pdb ..\inject_dll_x86.pdb /Y
 
@@ -30,7 +30,7 @@ cl -DUNICODE -D_UNICODE /EHsc /Zi /O1 /W3 /LD /MD /D BITS_64 /Qspectre run_code_
 copy run_code_on_dllmain_amd64.dll ..\run_code_on_dllmain_amd64.dll /Y
 copy run_code_on_dllmain_amd64.pdb ..\run_code_on_dllmain_amd64.pdb /Y
 
-cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /DEBUG /OPT:REF /OPT:ICF /GUARD:CF /out:inject_dll_amd64.exe
+cl /EHsc /Zi /O1 /W3 /Qspectre inject_dll.cpp /link /PROFILE /GUARD:CF /out:inject_dll_amd64.exe
 copy inject_dll_amd64.exe ..\inject_dll_amd64.exe /Y
 copy inject_dll_amd64.pdb ..\inject_dll_amd64.pdb /Y
 
