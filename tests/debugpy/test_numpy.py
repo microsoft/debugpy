df254f6aab6e4ae5ed364cb03881a102f2e1a461
Adam Yoblick <Adam.Yoblick@microsoft.com>
2024-07-09 16:10:42 -0500

    fix broken numpy assert for 3.8

--- tests/debugpy/test_numpy.py
+++ tests/debugpy/test_numpy.py
@@ -51,7 +51,7 @@ def test_ndarray(pyfile, target, run):
                 {
                     "type": "int32",
                     "name": "0",
-                    "value": "np.int32(123)",
+                    "value": some.str.containing("123"),
                     "variablesReference": some.int,
                 }
             ),
@@ -59,7 +59,7 @@ def test_ndarray(pyfile, target, run):
                 {
                     "type": "int32",
                     "name": "1",
-                    "value": "np.int32(456)",
+                    "value": some.str.containing("456"),
                     "variablesReference": some.int,
                 }
             ),
