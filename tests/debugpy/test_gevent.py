2f134962d8d6e8c1af4684c2cbc015b974a1aa5e
Rich Chiodo false <rchiodo@microsoft.com>
2024-10-09 15:55:09 -0700

    Fix gevent test to be skipped

--- tests/debugpy/test_gevent.py
+++ tests/debugpy/test_gevent.py
@@ -5,9 +5,9 @@
 import pytest
 from tests import debug
 from tests.patterns import some
-from tests_python.debugger_unittest import IS_PY313_OR_GREATER
+from _pydevd_bundle.pydevd_constants import IS_PY313_OR_GREATER
 
-pytest.mark.skipif(IS_PY313_OR_GREATER, reason="gevent is not up to date with 3.13 (_tstate_lock is not part of thread anymore)")
+@pytest.mark.skipif(IS_PY313_OR_GREATER, reason="gevent is not up to date with 3.13 (_tstate_lock is not part of thread anymore)")
 def test_gevent(pyfile, target, run):
     @pyfile
     def code_to_debug():
