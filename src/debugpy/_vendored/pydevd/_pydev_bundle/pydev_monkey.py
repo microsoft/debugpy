bd70738c28c94313f827ea45e31be57d1b01196a
lydiachalek <lydiaboyd30@gmail.com>
2025-04-02 11:04:50 -0700

    remove check for IS_PY38_OR_GREATER

--- src/debugpy/_vendored/pydevd/_pydev_bundle/pydev_monkey.py
+++ src/debugpy/_vendored/pydevd/_pydev_bundle/pydev_monkey.py
@@ -13,7 +13,6 @@ from _pydevd_bundle.pydevd_constants import (
     DebugInfoHolder,
     PYDEVD_USE_SYS_MONITORING,
     IS_PY313_OR_GREATER,
-    IS_PY38_OR_GREATER,
 )
 from _pydev_bundle import pydev_log
 from contextlib import contextmanager
@@ -1023,7 +1022,7 @@ def patch_new_process_functions():
     monkey_patch_os("spawnvpe", create_spawnve)
     monkey_patch_os("posix_spawn", create_posix_spawn)
 
-    if IS_PY38_OR_GREATER and not IS_WINDOWS:
+    if not IS_WINDOWS:
         monkey_patch_os("posix_spawnp", create_posix_spawn)
 
     if not IS_JYTHON:
