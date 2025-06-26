75e41b5918dab29815e55a6fa9d927008c4bac26
Conrad Irwin <conrad.irwin@gmail.com>
2025-06-26 11:18:21 -0600

    Use g++ on macOS (with -D_FORTIFY_SOURCE=2)

--- src/debugpy/_vendored/pydevd/pydevd_attach_to_process/linux_and_mac/compile_mac.sh
+++ src/debugpy/_vendored/pydevd/pydevd_attach_to_process/linux_and_mac/compile_mac.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-set -euxo pipefail
+set -e
+SRC="$(dirname "$0")/.."
 
-clang++ -fPIC -D_REENTRANT -std=c++11 -arch arm64 -c -o attach_arm64.o attach.cpp
-clang++ -dynamiclib -nostartfiles -arch arm64 -o attach_arm64.dylib attach_arm64.o -lc
-rm attach_arm64.o
+g++ -fPIC -D_REENTRANT -std=c++11 -D_FORTIFY_SOURCE=2 -arch arm64 -c -o "$SRC/attach_arm64.o" "$SRC/linux_and_mac/attach.cpp"
+g++ -dynamiclib -nostartfiles -arch arm64 -o "$SRC/attach_arm64.dylib" "$SRC/attach_arm64.o" -lc
+rm "$SRC/attach_arm64.o"
 
-clang++ -fPIC -D_REENTRANT -std=c++11 -arch x86_64 -c -o attach_x86_64.o attach.cpp
-clang++ -dynamiclib -nostartfiles -arch x86_64 -o attach_x86_64.dylib attach_x86_64.o -lc
-rm attach_x86_64.o
+g++ -fPIC -D_REENTRANT -std=c++11 -D_FORTIFY_SOURCE=2 -arch x86_64 -c -o "$SRC/attach_x86_64.o" "$SRC/linux_and_mac/attach.cpp"
+g++ -dynamiclib -nostartfiles -arch x86_64 -o "$SRC/attach_x86_64.dylib" "$SRC/attach_x86_64.o" -lc
+rm "$SRC/attach_x86_64.o"
 
-lipo -create attach_arm64.dylib attach_x86_64.dylib -output attach.dylib
-rm attach_arm64.dylib attach_x86_64.dylib
-mv attach.dylib ../attach.dylib
+lipo -create "$SRC/attach_arm64.dylib" "$SRC/attach_x86_64.dylib" -output "$SRC/attach.dylib"
+rm "$SRC/attach_arm64.dylib" "$SRC/attach_x86_64.dylib"
