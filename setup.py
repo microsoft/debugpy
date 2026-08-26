85da7b6bfe968210e3e39609eef016752d4436b5
Finn Womack <flan313@gmail.com>
2026-08-26 02:49:10 -0700

    add 3.15 to classifiers list in setup.py

--- setup.py
+++ setup.py
@@ -168,6 +168,7 @@ if __name__ == "__main__":
             "Programming Language :: Python :: 3.12",
             "Programming Language :: Python :: 3.13",
             "Programming Language :: Python :: 3.14",
+            "Programming Language :: Python :: 3.15",
             "Topic :: Software Development :: Debuggers",
             "Operating System :: Microsoft :: Windows",
             "Operating System :: MacOS",
